cbe9c1784c5b9f94cab2fd32a64deca0759b2809
garunitule <youtaiguhe@gmail.com>
2021-02-09 16:46:50 +0900

    サーバーの起動

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,13 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
 require('dotenv').config();
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+    console.log(`
+        Server is running!
+        Listening on port 4000
+        Explore at https://studio.apollographql.com/dev
+    `);
+});
