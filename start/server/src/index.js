ef45371b9e17e5b6d6369f68cef78d7e58353a19
garunitule <youtaiguhe@gmail.com>
2021-02-09 17:22:55 +0900

    データベースと連携

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,8 +1,19 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
-require('dotenv').config();
+const { createStore } = require('./utils');
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({
+    typeDefs,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store})
+    })
+});
 
 server.listen().then(() => {
     console.log(`
