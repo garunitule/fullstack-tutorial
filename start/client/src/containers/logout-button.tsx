b9af6d5c32bfa7c0de6619cb62749319d619a093
garunitule <youtaiguhe@gmail.com>
2021-02-11 14:27:12 +0900

    Mutation実装

--- start/client/src/containers/logout-button.tsx
+++ start/client/src/containers/logout-button.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import styled from 'react-emotion';
+import { useApolloClient } from '@apollo/client';
 
-const LogoutButton: React.FC<any> = () => {
-  return <div/>;
+import { menuItemClassName } from '../components/menu-item';
+import { isLoggedInVar } from '../cache';
+import { ReactComponent as ExitIcon } from '../assets/icons/exit.svg';
+
+const LogoutButton = () => {
+  const client = useApolloClient();
+  return (
+    <StyledButton
+      data-testid="logout-button"
+      onClick={() => {
+        client.cache.evict({ fieldName: 'me' });// 手動でオブジェクトを削除する
+        client.cache.gc();// 参照されていないオブジェクトを削除する. evictしたら他のオブジェクトも参照できなくなる可能性があるので、排除
+        localStorage.removeItem('token');
+        localStorage.removeItem('userItem');
+        isLoggedInVar(false);
+      }}
+    >
+      <ExitIcon />
+      Logout
+    </StyledButton>
+  );
 }
 
 export default LogoutButton;
+
+const StyledButton = styled('button')(menuItemClassName, {
+  background: 'none',
+  border: 'none',
+  padding: 0,
+});
